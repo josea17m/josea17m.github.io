03d75f49898e18ea0160503090ce6b1aa4d7e0f3
Josea M <92557972+josea17m@users.noreply.github.com>
2022-12-12 18:47:20 -0500

    cambios de colores, iconos skills

---
+++ JS/home.js
@@ -0,0 +1,1 @@
+home.addEventListener("click",(()=>{changes.removeDiCo(),changes.removeDiPr(),changes.removeDiSk(),changes.removePrBa(),changes.removeContactColor(),changes.removeSkillsColor(),changes.removeProyectColor(),changes.removeBackgroundI(),changes.removeBackgroundS(),changes.removeBackgroundP(),changes.removeBackgroundC(),changes.addBackgroundI()}));
